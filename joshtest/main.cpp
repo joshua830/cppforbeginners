d4b005beee928ea157b17dc87487b7c8a5c07c71
josh <praisegod59@gmail.com>
2017-06-13 14:56:12 -0400

    first commit

---
+++ joshtest/main.cpp
@@ -0,0 +1,22 @@
+// my first program by joshua quick
+
+#include <iostream>
+
+int main() {
+    std::cout << "hello i like cookies" << std::endl;
+
+    int cookie_limit =10;
+
+    for (int i = 0; i < cookie_limit; i++)
+    {
+        int num = i + 1;
+
+        std::cout << "jo" << num << " out of " << cookie_limit << " main " << std::endl;
+    }
+
+    return 0;
+}
+
+
+
+
